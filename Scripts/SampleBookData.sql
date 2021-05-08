254d95b2f16ede44086756757f14694e814dd01e
dkiskamp <47258708+dkiskamp@users.noreply.github.com>
2021-05-08 13:56:45 -0700

    New books
    Added 21 books to sample data

--- Scripts/SampleBookData.sql
+++ Scripts/SampleBookData.sql
@@ -97,3 +97,223 @@ CALL insert_book_author (
     "Barnes & Noble, Inc.",
     2009
 );
+
+CALL insert_book_author (
+    "9780679417392",
+    "Nineteen Eighty-Four",
+    7,
+    "George",
+    "Orwell",
+    "Secker & Warburg",
+    1949
+);
+
+CALL insert_book_author (
+    "9780743273565",
+    "The Great Gatsby",
+    5,
+    "F. Scott",
+    "Fitzgerald",
+    "Charles Scribner's Sons",
+    1925
+);
+
+CALL insert_book_author (
+    "9780446310789",
+    "To Kill a Mockingbird",
+    2,
+    "Harper",
+    "Lee",
+    "J. B. Lippincott & Co.",
+    1960
+);
+
+CALL insert_book_author (
+    "9787543321724",
+    "The Catcher in the Rye",
+    9,
+    "J.D.",
+    "Salinger",
+    "Little, Brown and Company",
+    1951
+);
+
+CALL insert_book_author (
+    "9780547928210",
+    "The Fellowship of the Ring",
+    8,
+    "J.R.R.",
+    "Tolkien",
+    "George Allen & Unwin",
+    1954
+);
+
+CALL insert_book_author (
+    "9780007203550",
+    "The Two Towers",
+    6,
+    "J.R.R.",
+    "Tolkein",
+    "George Allen & Unwin",
+    1954
+);
+
+CALL insert_book_author (
+    "9780345339737",
+    "The Return of the King",
+    4,
+    "J.R.R.",
+    "Tolkein",
+    "George Allen & Unwin",
+    1955
+);
+
+CALL insert_book_author (
+    "9780547928227",
+    "The Hobbit",
+    2,
+    "J.R.R.",
+    "Tolkein",
+    "George Allen & Unwin",
+    1937
+);
+
+CALL insert_book_author (
+    "9781503290563",
+    "Pride and Prejudice",
+    1,
+    "Jane",
+    "Austen",
+    "T. Egerton, Whitehall",
+    1813
+);
+
+CALL insert_book_author (
+    "9780684833392",
+    "Catch-22",
+    5,
+    "Joseph",
+    "Heller",
+    "Simon & Schuster",
+    1961
+);
+
+CALL insert_book_author (
+    "9781906388300",
+    "My 60 Memorable Games",
+    9,
+    "Bobby",
+    "Fischer",
+    "Simon & Schuster",
+    1969
+);
+
+CALL insert_book_author (
+    "9781503280786",
+    "Moby Dick",
+    3,
+    "Herman",
+    "Melville",
+    "Harper & Brothers",
+    1851
+);
+
+CALL insert_book_author (
+    "9780486280615",
+    "Adventures of Huckleberry Finn",
+    8,
+    "Mark",
+    "Twain",
+    "Chatto & Windus",
+    1884
+);
+
+CALL insert_book_author (
+    "9781503215672",
+    "The Adventures of Tom Sawyer",
+    4,
+    "Mark",
+    "Twain",
+    "American Publishing Company",
+    1876
+);
+
+CALL insert_book_author (
+    "9780142000663",
+    "The Grapes of Wrath",
+    1,
+    "John",
+    "Steinbeck",
+    "The Viking Press",
+    1939
+);
+
+CALL insert_book_author (
+    "9780486282114",
+    "Frankenstein",
+    8,
+    "Mary",
+    "Shelly",
+    "Lackington, Hughes, Harding, Mavor, & Jones",
+    1818
+);
+
+CALL insert_book_author (
+    "9781412811903",
+    "Animal Farm",
+    2,
+    "George",
+    "Orwell",
+    "Secker and Warburg",
+    1945
+);
+
+CALL insert_book_author (
+    "9780316055444",
+    "The Goldfinch",
+    10,
+    "Donna",
+    "Tartt",
+    "Little, Brown and Company",
+    2013
+);
+
+CALL insert_book_author (
+    "9781503222687",
+    "Alice's Adventures in Wonderland",
+    2,
+    "Lewis",
+    "Carroll",
+    "Macmillan",
+    1865
+);
+
+CALL insert_book_author (
+    "9781582187938",
+    "Through the Looking-Glass",
+    7,
+    "Lewis",
+    "Carroll",
+    "Macmillan",
+    1871
+);
+
+CALL insert_book_author (
+    "9780060293239",
+    "The Wonderful Wizard of OZ",
+    3,
+    "L. Frank",
+    "Baum",
+    "George M. Hill Company",
+    1900
+);
+
+CALL insert_book_author (
+    "9781450502955",
+    "Aesop's Fables",
+    8,
+    "Aesop",
+    NULL,
+    "Simon & Schuster",
+    1484
+);
