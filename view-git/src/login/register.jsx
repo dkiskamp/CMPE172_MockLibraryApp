6775f8620b90ac7f0e0b4ed936edbf993417c9ce
Tanmay Siwach <77550145+the-auk@users.noreply.github.com>
2021-05-08 20:39:30 -0700

    Add files via upload

--- view-git/src/login/register.jsx
+++ view-git/src/login/register.jsx
@@ -11,23 +11,25 @@ export class Register extends React.Component {
         <div className="header">Register</div>
         <div className="content">
           <div className="form">
-              <div className="form-group">
-              <label htmlFor="firstname">FirstName</label>
-              <input type="text" name="firstname" placeholder="firstname" />
-              <label htmlFor="lastanme">LastName</label>
-              <input type="text" name="lastname" placeholder="lastname" />
+            <div className="form-group">
               <label htmlFor="username">Username</label>
               <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
               <label htmlFor="email">Email</label>
               <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="form-group">
               <label htmlFor="password">Password</label>
               <input type="text" name="password" placeholder="password" />
             </div>
+          </div>
+        </div>
+        <div className="footer">
           <button type="button" className="btn">
             Register
           </button>
-          </div>
-          </div>
+        </div>
       </div>
     );
   }
